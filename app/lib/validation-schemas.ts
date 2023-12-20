63f156a50804b379fad53a4908947e80846f336f
SalamSki <slome.lord123@gmail.com>
2023-12-20 14:43:52 +0100

    Usename Validation numbers and letters

--- app/lib/validation-schemas.ts
+++ app/lib/validation-schemas.ts
@@ -1,19 +1,22 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 export const emailSchema = z.string().email();
 
 export const userSchema = z
   .string()
-  .min(3, { message: 'At least 3 characters long.' })
-  .max(25, { message: 'At most 25 characters long.' })
-  .refine((s) => !s.includes(' '), 'No Spaces.');
+  .min(3, { message: "At least 3 characters long." })
+  .max(25, { message: "At most 25 characters long." })
+  .regex(RegExp("^[A-Za-z0-9]{3,}$"), {
+    message: "Only letters and numbers allowed",
+  })
+  .refine((s) => !s.includes(" "), "No Spaces.");
 
 export const passSchema = z
   .string()
-  .regex(RegExp('.*[A-Z].*'), { message: 'At least one capital letter.' })
-  .regex(RegExp('.*[0-9].*'), { message: 'At least one number.' })
+  .regex(RegExp(".*[A-Z].*"), { message: "At least one capital letter." })
+  .regex(RegExp(".*[0-9].*"), { message: "At least one number." })
   .regex(RegExp(/.*[?#!@$%^&*(){}\\=_>€`~<;"|'\/:,.+\[\]-].*/), {
-    message: 'At lesat one speical character.',
+    message: "At lesat one speical character.",
   })
-  .min(8, { message: 'At least 8 characters long.' })
-  .refine((s) => !s.includes(' '), 'No Spaces.');
+  .min(8, { message: "At least 8 characters long." })
+  .refine((s) => !s.includes(" "), "No Spaces.");
