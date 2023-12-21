21c579953e04c29b6e8f88507d41a38629ba55ec
SalamSki <slome.lord123@gmail.com>
2023-12-21 19:37:00 +0100

    Update prisma and disable pwa in dev.

--- next.config.js
+++ next.config.js
@@ -5,5 +5,6 @@ const withPWA = require("next-pwa")({
   register: true,
   skipWaiting: true,
   disableDevLogs: true,
+  disable: process.env.NODE_ENV !== "production",
 });
 module.exports = withPWA(nextConfig);
